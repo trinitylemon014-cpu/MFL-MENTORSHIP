de288dad501dc873ae628b4915d09760b1b72aed
trinitylemon014-cpu <trinitylemon014@gmail.com>
2026-07-16 13:17:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1353,4 +1353,4 @@ app.register_blueprint(stories_bp)
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, debug=False, host='0.0.0.0', port=port)
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
